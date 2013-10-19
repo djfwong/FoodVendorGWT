2f9b303fa3ee0476e64b8b21251a21fd0677b124
Fanny Wong <djfwong@gmail.com>
2013-10-18 23:55:58 -0700

    using temp csv parser which requires self modification of a xls file to read contents of xls file

--- src/com/sneakyxpress/webapp/server/FoodVendor.java
+++ src/com/sneakyxpress/webapp/server/FoodVendor.java
@@ -1,5 +1,7 @@
 package com.sneakyxpress.webapp.server;
 
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,10 +12,12 @@ public class FoodVendor {
 	/**
 	 * FoodVendor domain object
 	 */
+	
 
 	// Unique identifier for object
+	@PrimaryKey
+	@Persistent
 	@Id
-	@Column(name = "key")
 	private String vendorId;
 
 	// Business name
@@ -35,4 +39,52 @@ public class FoodVendor {
 	// Latitude coordinate
 	@Column(name = "latitude")
 	private double latitude;
+
+	public void setVendorId(String vendorId) {
+		this.vendorId = vendorId;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getVendorId() {
+		return vendorId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
 }
