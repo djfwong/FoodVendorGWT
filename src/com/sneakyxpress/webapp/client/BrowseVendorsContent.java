0a99892b8500cb889cbdf465bdac400c44e0d578
Fanny Wong <djfwong@gmail.com>
2013-10-25 23:05:11 -0700

    displays vendors in static table no sort functionality yet, although it takes up entire page

--- src/com/sneakyxpress/webapp/client/BrowseVendorsContent.java
+++ src/com/sneakyxpress/webapp/client/BrowseVendorsContent.java
@@ -1,92 +1,154 @@
 package com.sneakyxpress.webapp.client;
 
+import java.util.List;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Anchor;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.ListDataProvider;
 import com.sneakyxpress.webapp.shared.FoodVendor;
-
-import java.util.List;
 
 /**
  * The contents of the Browse Vendors page
  */
 public class BrowseVendorsContent extends Content {
-    private static final String pageName = "Browse Vendors";
-    private static final String pageStub = "vendors";
+	private static final String pageName = "Browse Vendors";
+	private static final String pageStub = "vendors";
 
-    private final BrowseVendorsServiceAsync browseVendorsService = GWT.create(BrowseVendorsService.class);
+	private final BrowseVendorsServiceAsync browseVendorsService = GWT
+			.create(BrowseVendorsService.class);
 
-    public BrowseVendorsContent(Sneaky_Xpress module) {
-        super(module);
-    }
+	public BrowseVendorsContent(Sneaky_Xpress module) {
+		super(module);
+	}
 
-    @Override
-    public String getPageName() {
-        return pageName;
-    }
+	@Override
+	public String getPageName() {
+		return pageName;
+	}
 
-    @Override
-    public String getPageStub() {
-        return pageStub;
-    }
+	@Override
+	public String getPageStub() {
+		return pageStub;
+	}
 
-    @Override
-    public void getAndChangeContent(final String input) {
-        browseVendorsService.browseVendorsServer(input, new AsyncCallback<List<FoodVendor>>() {
-            public void onFailure(Throwable caught) {
-                module.addMessage(GENERIC_ERROR_MESSAGE);
-            }
+	@Override
+	public void getAndChangeContent(final String input) {
+		browseVendorsService.browseVendorsServer(input,
+				new AsyncCallback<List<FoodVendor>>() {
+					public void onFailure(Throwable caught) {
+						module.addMessage(GENERIC_ERROR_MESSAGE);
+					}
 
-            public void onSuccess(List<FoodVendor> result) {
-                HTMLPanel content = new HTMLPanel(""); // The new content to return
+					public void onSuccess(List<FoodVendor> result) {
+						HTMLPanel content = new HTMLPanel(""); // The new
+																// content to
+																// return
 
-                // The sub-navigation bar
-                HTMLPanel list = new HTMLPanel("ul", "");
-                list.addStyleName("nav nav-tabs");
+						// The sub-navigation bar
+						HTMLPanel list = new HTMLPanel("ul", "");
+						list.addStyleName("nav nav-tabs");
 
-                // The tabs in the sub-navigation bar
-                HTMLPanel listView = new HTMLPanel("li", "");
-                HTMLPanel mapView = new HTMLPanel("li", "");
+						// The tabs in the sub-navigation bar
+						HTMLPanel listView = new HTMLPanel("li", "");
+						HTMLPanel mapView = new HTMLPanel("li", "");
 
-                // Links for the tabs
-                Anchor listViewLink = new Anchor("List View");
-                listViewLink.addClickHandler(new PageClickHandler(BrowseVendorsContent.this, "list"));
-                listView.add(listViewLink);
+						// Links for the tabs
+						Anchor listViewLink = new Anchor("List View");
+						listViewLink.addClickHandler(new PageClickHandler(
+								BrowseVendorsContent.this, "list"));
+						listView.add(listViewLink);
 
-                Anchor mapViewLink = new Anchor("Map View");
-                mapViewLink.addClickHandler(new PageClickHandler(BrowseVendorsContent.this, "map"));
-                mapView.add(mapViewLink);
+						Anchor mapViewLink = new Anchor("Map View");
+						mapViewLink.addClickHandler(new PageClickHandler(
+								BrowseVendorsContent.this, "map"));
+						mapView.add(mapViewLink);
 
+						// Create the sub-navigation bar
+						list.add(listView);
+						list.add(mapView);
+						content.add(list);
 
-                // Create the sub-navigation bar
-                list.add(listView);
-                list.add(mapView);
-                content.add(list);
+						// Load the appropriate view
+						if (input.equals("map")) {
+							mapView.addStyleName("active");
+							listView.removeStyleName("active");
+							// Add the map etc.
+						} else {
+							// By default load the list view
+							listView.addStyleName("active");
+							mapView.removeStyleName("active");
 
-                // Load the appropriate view
-                if (input.equals("map")) {
-                    mapView.addStyleName("active");
-                    listView.removeStyleName("active");
-                    // Add the map etc.
-                } else {
-                    // By default load the list view
-                    listView.addStyleName("active");
-                    mapView.removeStyleName("active");
+							// Add data table under List view
+							Widget table = displayDataInTable(result);
+							listView.add(table);
+						}
 
-                    HTMLPanel vendors = new HTMLPanel("");
-                    for (FoodVendor v : result) {
-                        HTML a = new HTML();
-                        a.setHTML("<p>" + v.getName() + "</p>");
-                        vendors.add(a);
-                    }
+						module.changeContent(content);
+					}
+				});
+	}
 
-                    content.add(vendors);
-                }
+	public Widget displayDataInTable(List<FoodVendor> vendorList) {
+		// Create new cell table object
+		CellTable<FoodVendor> table = new CellTable<FoodVendor>();
 
-                module.changeContent(content);
-            }
-        });
-    }
+		// Configure table to display all results onto one page
+		table.setPageSize(vendorList.size());
+
+		// Add Key column
+		TextColumn<FoodVendor> keyCol = new TextColumn<FoodVendor>() {
+			@Override
+			public String getValue(FoodVendor vendor) {
+				return vendor.getVendorId();
+			}
+		};
+		table.addColumn(keyCol, "Key");
+
+		// Add Name column
+		TextColumn<FoodVendor> nameCol = new TextColumn<FoodVendor>() {
+			@Override
+			public String getValue(FoodVendor vendor) {
+				return vendor.getName();
+			}
+		};
+		table.addColumn(nameCol, "Name");
+
+		// Add Location column
+		TextColumn<FoodVendor> locCol = new TextColumn<FoodVendor>() {
+			@Override
+			public String getValue(FoodVendor vendor) {
+				return vendor.getLocation();
+			}
+		};
+		table.addColumn(locCol, "Location");
+
+		// Add Description column
+		TextColumn<FoodVendor> desCol = new TextColumn<FoodVendor>() {
+			@Override
+			public String getValue(FoodVendor vendor) {
+				return vendor.getDescription();
+			}
+		};
+		table.addColumn(desCol, "Description");
+
+		// Create a list data provider.
+		final ListDataProvider<FoodVendor> dataProvider = new ListDataProvider<FoodVendor>();
+
+		// Add the cellList to the dataProvider.
+		dataProvider.addDataDisplay(table);
+
+		// Set to query result
+		dataProvider.setList(vendorList);
+
+		// return table and display on page
+		return table;
+
+	}
+
 }
