5f1045504590b33565e46e92fe48cb5b2e987ad5
Fanny Wong <djfwong@gmail.com>
2013-10-28 10:20:01 -0700

    re-added back cell table for list view of food vendors

---
+++ src/com/sneakyxpress/webapp/client/DesComparator.java
@@ -0,0 +1,20 @@
+package com.sneakyxpress.webapp.client;
+
+import java.util.Comparator;
+
+import com.sneakyxpress.webapp.shared.FoodVendor;
+
+public class DesComparator implements Comparator<FoodVendor> {
+	public int compare(FoodVendor o1, FoodVendor o2) {
+		if (o1 == o2) {
+			return 0;
+		}
+
+		// Compare the title columns.
+		if (o1 != null) {
+			return (o2 != null) ? o1.getDescription().compareTo(
+					o2.getDescription()) : 1;
+		}
+		return -1;
+	}
+}
---
+++ src/com/sneakyxpress/webapp/client/LocComparator.java
@@ -0,0 +1,20 @@
+package com.sneakyxpress.webapp.client;
+
+import java.util.Comparator;
+
+import com.sneakyxpress.webapp.shared.FoodVendor;
+
+public class LocComparator implements Comparator<FoodVendor> {
+	public int compare(FoodVendor o1, FoodVendor o2) {
+		if (o1 == o2) {
+			return 0;
+		}
+
+		// Compare the title columns.
+		if (o1 != null) {
+			return (o2 != null) ? o1.getLocation().compareTo(o2.getLocation())
+					: 1;
+		}
+		return -1;
+	}
+}
