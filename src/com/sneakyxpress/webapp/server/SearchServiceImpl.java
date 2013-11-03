bc105b1f1b9336569f8f777410c3258be50137e9
Rafael Malcolm <raf_malcolm@yahoo.ca>
2013-11-02 21:47:35 -0700

    search done

--- src/com/sneakyxpress/webapp/server/SearchServiceImpl.java
+++ src/com/sneakyxpress/webapp/server/SearchServiceImpl.java
@@ -20,68 +20,20 @@ public class SearchServiceImpl extends RemoteServiceServlet implements
     	
     	List<FoodVendor> search_results = new ArrayList<FoodVendor>();
     	PersistenceManager pm = PMF.get().getPersistenceManager();
-    	String queryString1 = String.format("name.startsWith('%s')", input);
-		Query q1 = pm.newQuery(FoodVendor.class, queryString1);
-		
-		try {
-			List<FoodVendor> results = (List<FoodVendor>) q1.execute();
-			if (!results.isEmpty()) {
-				for (FoodVendor f: results) {
-					// process result f
-					search_results.add(f);
-				}
-			} else {
-			}
-		} finally {
-				q1.closeAll();
-				}
-		
-		String queryString2 = String.format("location.startsWith('%s')", input);
-		Query q2 = pm.newQuery(FoodVendor.class, queryString2);
-		
-		try {
-			List<FoodVendor> results = (List<FoodVendor>) q2.execute();
-			if (!results.isEmpty()) {
-				for (FoodVendor f: results) {
-					// process result f
-					if (!containedInResults(search_results, f)){
-					search_results.add(f);
-					}
-				}
-			} else {
-			}
-		} finally {
-				q2.closeAll();
-				}
-		
-		String queryString3 = String.format("description.startsWith('%s')", input);
-		Query q3 = pm.newQuery(FoodVendor.class, queryString3);
-		
-		try {
-			List<FoodVendor> results = (List<FoodVendor>) q3.execute();
-			if (!results.isEmpty()) {
-				for (FoodVendor f: results) {
-					// process result f
-					if (!containedInResults(search_results, f)){
-					search_results.add(f);
-					}
-				}
-			} else {
-			}
-		} finally {
-				q3.closeAll();
-				}
-
+    	Query q1 = pm.newQuery(FoodVendor.class);
+    	List<FoodVendor> all_vendors = (List<FoodVendor>) q1.execute();
+    	for (FoodVendor f : all_vendors){
+    		if (f.getName().contains(input)){
+    			search_results.add(f);
+    		} else if (f.getDescription().contains(input)){
+    			search_results.add(f);
+    		} else if (f.getLocation().contains(input)){
+    			search_results.add(f);
+    		}
+    	}
+    	
 		return search_results;
     	
-        //return "<h1>Search results (stub) for input: " + input + "</h1>";
     }
     
-    private boolean containedInResults(List<FoodVendor> search_results, FoodVendor f){
-    	for (FoodVendor fv : search_results){
-    		if (f.getVendorId() == fv.getVendorId()){
-    			return true;
-    		}
-    	} return false;
-    }
 }
