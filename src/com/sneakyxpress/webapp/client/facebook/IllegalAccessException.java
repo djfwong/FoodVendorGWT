36df60ad630da5728cfea3916a7e4487ea70ea2d
Michael Thiesen <micthiesen@gmail.com>
2013-11-07 12:20:50 -0800

    Forgot an exception class.

---
+++ src/com/sneakyxpress/webapp/client/facebook/IllegalAccessException.java
@@ -0,0 +1,10 @@
+package com.sneakyxpress.webapp.client.facebook;
+
+public class IllegalAccessException extends Exception {
+    public IllegalAccessException() {
+    }
+
+    public IllegalAccessException(String message) {
+        super(message);
+    }
+}
